6128dcddcc01d5726cd831f75ecffdfce9b9abc9
Katrina Fuller <trinafuller@att.net>
2019-09-04 13:51:11 -0600

    made car model in service

---
+++ server/services/CarService.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+const _model = new Schema({
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  imgUrl: { type: String, default: 'https://placehold.it/200x200' },
+  price: { type: Number, required: true },
+  year: { type: Number, required: true },
+  description: { type: String, required: true }
+}, { timestamps: true })
+
+export default class CarService {
+  get repository() {
+    return mongoose.model('value', _model)
+  }
+}
